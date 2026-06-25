f438486b0e7c7f6b3cf27fc3511729287f3a1247
gastonminetti <gastonminetti4@gmail.com>
2026-06-25 14:14:38 -0300

    Refactor geofence filter to use external pointInPolygon function; enhance operator service with alert handling and zone detection

--- transportation-service/src/modules/alerts/pipeline/filters/geofence.filter.ts
+++ transportation-service/src/modules/alerts/pipeline/filters/geofence.filter.ts
@@ -2,20 +2,7 @@ import type { GpsSignalDTO } from "@move/shared";
 import type { ISignalFilter } from "../filter.interface";
 import { acquireAlertLock, createAlert } from "../../service";
 import { ZoneModel } from "../../../../db/models";
-
-function pointInPolygon(point: [number, number], ring: number[][]): boolean {
-  const [px, py] = point;
-  let inside = false;
-  for (let i = 0, j = ring.length - 1; i < ring.length; j = i++) {
-    const xi = ring[i]![0]!;
-    const yi = ring[i]![1]!;
-    const xj = ring[j]![0]!;
-    const yj = ring[j]![1]!;
-    const intersect = yi > py !== yj > py && px < ((xj - xi) * (py - yi)) / (yj - yi) + xi;
-    if (intersect) inside = !inside;
-  }
-  return inside;
-}
+import { pointInPolygon } from "../../../zones/geo";
 
 export class GeofenceFilter implements ISignalFilter {
   readonly name = "geofence";
